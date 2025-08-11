a3a9d7b45a3d578868f55a2fededb5257092b762
AnshikaS156 <saraswat.anshika156@gmail.com>
2025-08-11 15:31:20 +0530

    commit

--- hello.java
+++ hello.java
@@ -2,5 +2,11 @@ public class hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+
+    System.out.println("This is a simple Java program.");
+
+    System.out.println("Welcome to Java programming!");
     
+
 }
